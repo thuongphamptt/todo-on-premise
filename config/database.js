b090edf50b5f79e70f47b47c94a71aa82b543c21
Xuan An Vo <xuanan2.vo@mafc.com.vn>
2023-09-19 20:15:11 +0700

    a

--- config/database.js
+++ config/database.js
@@ -1,15 +1,40 @@
-// This file is to connect to mysql database
-
+// This file is for connecting to the MySQL database
 require('dotenv').config();
 const mysql = require('mysql');
-
 const keys = require('./keys');
 
 const connection = mysql.createConnection(keys.mysql);
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log('connected!');
+  if (err) {
+    console.error('Error connecting to the database: ' + err.message);
+    return;
+  }
+
+  console.log('Connected to the database!');
+
+  let createTodos = `CREATE TABLE IF NOT EXISTS todos (
+    todo_title VARCHAR(255) NOT NULL,
+    todo_body VARCHAR(255) NOT NULL,
+    is_completed TINYINT(1) NOT NULL DEFAULT 0
+  )`;
+
+  connection.query(createTodos, function(err, results, fields) {
+    if (err) {
+      console.error('Error creating the todos table: ' + err.message);
+      return;
+    }
+
+    console.log('Todos table created or already exists.');
+
+    // connection.end(function(err) {
+    //   if (err) {
+    //     console.error('Error closing the database connection: ' + err.message);
+    //     return;
+    //   }
+    //   console.log('Database connection closed.');
+    // });
+  });
 });
 
 module.exports = {
