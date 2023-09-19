8df0f854311067d0e62d276c4c32cd86b22d1a6d
Xuan An Vo <xuanan2.vo@mafc.com.vn>
2023-09-19 19:44:28 +0700

    a

--- config/database.js
+++ config/database.js
@@ -8,7 +8,27 @@ const keys = require('./keys');
 const connection = mysql.createConnection(keys.mysql);
 
 connection.connect(function(err) {
-  if (err) throw err;
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+
+  let createTodos = `create table if not exists todos(
+                          todo_title varchar(255)not null,
+                          todo_body varchar(255)not null,
+                          is_completed tinyint(1) not null default 0
+                      )`;
+
+  connection.query(createTodos, function(err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    }
+  });
+
+  connection.end(function(err) {
+    if (err) {
+      return console.log(err.message);
+    }
+  });
   console.log('connected!');
 });
 
